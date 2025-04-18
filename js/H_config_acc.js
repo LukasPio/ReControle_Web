788c5ced768b254b797dec2ae447c750cef79a1a
SchiavoEnzo <enzoschiavo238@gmail.com>
2025-04-18 20:05:52 -0300

    bug fixes

--- js/H_config_acc.js
+++ js/H_config_acc.js
@@ -1,10 +1,10 @@
 import { initializeApp }  from   'https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js';
-import { getAuth, signInWithEmailAndPassword, updateProfile /*, updatePassword*/} from   'https://www.gstatic.com/firebasejs/11.3.1/firebase-auth.js';
+import { getAuth, signInWithEmailAndPassword, updateProfile/*, updateEmail, updatePassword*/} from   'https://www.gstatic.com/firebasejs/11.3.1/firebase-auth.js';
 import {firebaseConfig, hrefsConfig} from "./Config.js";
 
 const perfil = document.getElementById("perfil");
 const back = document.getElementById("back");
-//const seguranca = document.getElementById("seguranca");
+const seguranca = document.getElementById("seguranca");
 const tema = document.getElementById("tema");
 //const aba4 = document.getElementById(""); Pode ou não ser inserido
 //const aba5 = document.getElementById(""); Pode ou não ser inserido
@@ -64,15 +64,8 @@ document.addEventListener('DOMContentLoaded', () => {
               <button id='hide_show'><svg xmlns="http://www.w3.org/2000/svg" height="10px" viewBox="0 -960 960 960" width="24px" fill="#000"><path d="m644-428-58-58q9-47-27-88t-93-32l-58-58q17-8 34.5-12t37.5-4q75 0 127.5 52.5T660-500q0 20-4 37.5T644-428Zm128 126-58-56q38-29 67.5-63.5T832-500q-50-101-143.5-160.5T480-720q-29 0-57 4t-55 12l-62-62q41-17 84-25.5t90-8.5q151 0 269 83.5T920-500q-23 59-60.5 109.5T772-302Zm20 246L624-222q-35 11-70.5 16.5T480-200q-151 0-269-83.5T40-500q21-53 53-98.5t73-81.5L56-792l56-56 736 736-56 56ZM222-624q-29 26-53 57t-41 67q50 101 143.5 160.5T480-280q20 0 39-2.5t39-5.5l-36-38q-11 3-21 4.5t-21 1.5q-75 0-127.5-52.5T300-500q0-11 1.5-21t4.5-21l-84-82Zm319 93Zm-151 75Z"/></svg></button>
               <button id='edit_3'>Salvar</button><button id=back_3>Reverter</button></p>`;                                                                                 
 
-              if (C_user.emailVerified) {
-                
-                document.getElementById("change").innerHTML += "<br><p> Email verificado. </p>";
-                
-              }
+        
 
-              
-
-              
                   perfil.addEventListener('click', () => {
       
                     document.getElementById("change").innerHTML = `<p><input type='text' id='in_1' value='${C_user.displayName}'>
@@ -85,22 +78,20 @@ document.addEventListener('DOMContentLoaded', () => {
                               <button id='hide_show'><svg xmlns="http://www.w3.org/2000/svg" height="10px" viewBox="0 -960 960 960" width="24px" fill="#000"><path d="m644-428-58-58q9-47-27-88t-93-32l-58-58q17-8 34.5-12t37.5-4q75 0 127.5 52.5T660-500q0 20-4 37.5T644-428Zm128 126-58-56q38-29 67.5-63.5T832-500q-50-101-143.5-160.5T480-720q-29 0-57 4t-55 12l-62-62q41-17 84-25.5t90-8.5q151 0 269 83.5T920-500q-23 59-60.5 109.5T772-302Zm20 246L624-222q-35 11-70.5 16.5T480-200q-151 0-269-83.5T40-500q21-53 53-98.5t73-81.5L56-792l56-56 736 736-56 56ZM222-624q-29 26-53 57t-41 67q50 101 143.5 160.5T480-280q20 0 39-2.5t39-5.5l-36-38q-11 3-21 4.5t-21 1.5q-75 0-127.5-52.5T300-500q0-11 1.5-21t4.5-21l-84-82Zm319 93Zm-151 75Z"/></svg></button>
                               <button id='edit_3'>Salvar</button><button id=back_3>Reverter</button></p>`;
 
-                              if (C_user.emailVerified) {
-                
-                                document.getElementById("change").innerHTML += "<br><p> Email verificado. </p>";
-                                
-                              }
+                             
                 })    
       
                 if (tema) {      tema.addEventListener('click', () => {
                    document.getElementById("change").innerHTML = `<h2>Tema</h2>
-                   <select id='t_select'><option id='light' value='light-mode'>Branco</option><option id='dark' value='dark-mode'>Preto</option><option id='gray' value='gray-mode'>Cinza</option></select>`;
+                   <select id='t_select'><option id='light' value='light-mode'>Branco</option><option id='dark' value='dark-mode'>Preto</option><option id='gray' value='gray-mode'>Cinza</option></select>
+                   <br><br><input type='color' id='t_color'>`;
                    /*O branco será o primeiramente selecionado atualmente pelo localstorage*/
                    
                    const light = document.getElementById("light");
                    const gray = document.getElementById("gray");
                    const dark  = document.getElementById("dark");
                    const select = document.getElementById("t_select");
+                   const sel_color = document.getElementById("t_color");
                    
                    if (select) {select.addEventListener('change', (event) => {
                     
@@ -118,8 +109,27 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
                     })}
+
+                   if (sel_color) { sel_color.addEventListener('change', (event) => {
+                      const hexColor = event.target.value;
+                      const elements = document.getElementById('change');
+                      document.getElementById("change").innerHTML += " " + hexColor + "- Em andamento...";
+                      
+                    })}
                 }) 
                  
+                }
+
+                if (seguranca) {   seguranca.addEventListener('click', () => {
+
+                  if (C_user.emailVerified) {
+                
+                    document.getElementById("change").innerHTML = "<br><p> Email verificado. </p>";
+                    
+                  }
+
+                })
+
                 }
 
                 if (back) {  back.addEventListener('click', () => {
@@ -164,7 +174,7 @@ document.addEventListener('DOMContentLoaded', () => {
           const passwordInput = document.getElementById('in_3');
 
           
-          
+          // Salvar nome
           if (edit_1) {
             edit_1.addEventListener('click', () => {
               if (updateProfile(C_user, {displayName: document.getElementById("in_1").value})) {
@@ -173,16 +183,18 @@ document.addEventListener('DOMContentLoaded', () => {
             })
           }
 
-
+          // Salvar email
           if (edit_2) {
             edit_2.addEventListener('click', () => {
-              if (updateProfile(C_user, {email: document.getElementById("in_2").value})) {
+              /*const user = userCredential.user;
+              if (user.updateEmail(document.getElementById('in_2').value)) {
                 console.log("Alterado com sucesso.")
-              }
+              }*/
+             alert("Em andamento.");
             })
           }
 
-
+          // Salvar senha
           if(edit_3) {
             edit_3.addEventListener('click', () => {
              /* if (auth.currentUser.updatePassword(document.getElementById("in_3").value)) {
@@ -192,7 +204,7 @@ document.addEventListener('DOMContentLoaded', () => {
             })
           }
 
-
+          // Reverter nome
           if (back_1) {
             back_1.addEventListener('click', () => {
 
@@ -204,7 +216,7 @@ document.addEventListener('DOMContentLoaded', () => {
             })
           }
 
-
+          // Reverter email
           if (back_2) {
             back_2.addEventListener('click', () => {
 
@@ -216,7 +228,7 @@ document.addEventListener('DOMContentLoaded', () => {
             })
           }
 
-
+          // Reverter senha
           if (back_3) {
             back_3.addEventListener('click', () => {
 
@@ -225,9 +237,6 @@ document.addEventListener('DOMContentLoaded', () => {
               alert("Reversão em andamento...");
           })
         }
-
-        
-
   
         if (pass_in){
         pass_in.addEventListener('click', () => {
