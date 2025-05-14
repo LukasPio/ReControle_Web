71ea2633e379f548feb51b6e489901ca5c5773f5
SchiavoEnzo <enzoschiavo238@gmail.com>
2025-05-14 20:28:01 -0300

    cadastro

---
+++ js/index.js
@@ -0,0 +1,7 @@
+import Swal from '../node_modules/sweetalert2/dist/sweetalert2.all.min.js';
+
+Swal.fire({
+    title: 'Corrigido!',
+    text: 'Agora o import está funcionando!',
+    icon: 'success'
+});
