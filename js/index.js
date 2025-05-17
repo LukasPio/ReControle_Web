dcc374f574c03002def400521b60996d60005c0f
LukasPio <lucaspio.galvao@gmail.com>
2025-05-17 18:49:13 -0300

    add sweetalert2 cdn dependency

--- js/index.js
+++
@@ -1,7 +0,0 @@
-import Swal from '../node_modules/sweetalert2/dist/sweetalert2.all.min.js';
-
-Swal.fire({
-    title: 'Corrigido!',
-    text: 'Agora o import está funcionando!',
-    icon: 'success'
-});
